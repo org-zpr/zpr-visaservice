76bcb8df2add9a27cee8a781f6684577bacccecf
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2025-09-22 19:15:50 -0400

    boilerplate

--- zpt/src/main.rs
+++ zpt/src/main.rs
@@ -2,21 +2,28 @@ use clap::Parser;
 use colored::Colorize;
 use std::path::PathBuf;
 
+use libeval::zpr_policy::ZprPolicy;
+
 #[derive(Parser, Debug)]
 #[command(name = "zpt")]
 #[command(version, verbatim_doc_comment)]
 struct Cli {
     /// Path to a compiled ZPR policy.
-    #[arg(value_name = "POLICY_BINARY")]
+    #[arg(short, long, value_name = "POLICY_BINARY")]
     policy: PathBuf,
 }
 
 fn main() {
-    let exit_code = 0;
     let cli = Cli::parse();
     println!(
         "Policy binary path: {}",
-        cli.policy.display().to_string().green()
+        cli.policy.to_string_lossy().green()
     );
-    std::process::exit(exit_code);
+
+    let _zp = ZprPolicy::new_from_file(&cli.policy).unwrap_or_else(|e| {
+        eprintln!("{}: {e}", "Error loading policy".red());
+        std::process::exit(1);
+    });
+
+    std::process::exit(0);
 }
