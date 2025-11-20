aacd08dfae766d147a49f41f2d5dc179855596cc
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2025-11-20 12:17:44 -0500

    org

--- vs-dt/src/vsapi_types.rs
+++ vs-dt/src/vsapi_types.rs
@@ -1,3 +1,5 @@
+//! API types that are converted into/out-of Capn Proto.
+
 use ::zpr::vsapi::v1 as vsapi;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
@@ -27,6 +29,7 @@ pub enum EndpointT {
 
 pub enum Constraint {}
 
+#[allow(dead_code)]
 pub struct KeySet {
     ingress: Vec<u8>,
     egress: Vec<u8>,
@@ -41,6 +44,32 @@ pub enum VisaDenialReason {
     SourceAuthError,
     DestAuthError,
     QuotaExceeded,
+}
+
+/// A description of a packet between a sender and reciever.
+#[allow(dead_code)]
+pub struct PacketDesc {
+    pub source_addr: IpAddr,
+    pub dest_addr: IpAddr,
+    pub protocol: u8,
+    /// Source port or ICMP type
+    pub source_port: u16,
+    /// Destination port or ICMP code
+    pub dest_port: u16,
+    /// TODO: Can multiple flags be passed with the PacketDesc?
+    pub comm_flags: CommFlag,
+}
+
+/// Special hint that is passed with a [PacketDesc].
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum CommFlag {
+    /// TODO: document this
+    BiDirectional,
+    /// TODO: document this
+    UniDirectional,
+    /// Is-a re-request, includes previous visa id.
+    /// TODO: Hmm... Does this assumes a lot of state accessible to the evaluator?
+    ReRequest(u64),
 }
 
 fn write_ip_addr(bldr: &mut vsapi::ip_addr::Builder<'_>, ip: &IpAddr) {
@@ -172,32 +201,6 @@ impl TryFrom<vsapi::packet_desc::Reader<'_>> for PacketDesc {
             comm_flags,
         })
     }
-}
-
-/// A description of a packet between a sender and reciever.
-#[allow(dead_code)]
-pub struct PacketDesc {
-    pub source_addr: IpAddr,
-    pub dest_addr: IpAddr,
-    pub protocol: u8,
-    /// Source port or ICMP type
-    pub source_port: u16,
-    /// Destination port or ICMP code
-    pub dest_port: u16,
-    /// TODO: Can multiple flags be passed with the PacketDesc?
-    pub comm_flags: CommFlag,
-}
-
-/// Special hint that is passed with a [PacketDesc].
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum CommFlag {
-    /// TODO: document this
-    BiDirectional,
-    /// TODO: document this
-    UniDirectional,
-    /// Is-a re-request, includes previous visa id.
-    /// TODO: Hmm... Does this assumes a lot of state accessible to the evaluator?
-    ReRequest(u64),
 }
 
 impl PacketDesc {
