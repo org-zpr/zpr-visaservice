754af98bf502351d07b09f24fb83056f2356428a
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2026-07-28 11:16:12 -0400

    patch up test

--- integration-test/zpt-test-connect.sh
+++ integration-test/zpt-test-connect.sh
@@ -10,9 +10,19 @@ PROG_CMD=("$ZPT_BIN" -i "$INPUT" --json)
 obj1="$("${PROG_CMD[@]}" | sed -n '1p')"
 obj2="$("${PROG_CMD[@]}" | sed -n '2p')"
 
-echo "TESTING CONNECT SHUOULD SUCCEED"
-jq -e '.kind=="APPROVE_CONNECTION" and .actor.provider==true' >/dev/null <<<"$obj1"
+echo "TESTING CONNECT SHOULD SUCCEED AS A SERVICE PROVIDER"
+jq -e \
+    '.kind == "APPROVE_CONNECTION"
+     and .actor.attrs["zpr.services"].value == ["bas"]' \
+    >/dev/null <<<"$obj1"
+echo "TEST OK"
+
+echo "TESTING CONNECT WITHOUT A MATCHING JOIN POLICY SHOULD SUCCEED AS AN ADAPTER"
+jq -e \
+    '.kind == "APPROVE_CONNECTION"
+     and .actor.attrs["zpr.role"].value == ["adapter"]
+     and (.actor.attrs | has("zpr.services") | not)' \
+    >/dev/null <<<"$obj2"
 echo "TEST OK"
 
 echo "OK"
-
